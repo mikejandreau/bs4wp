b3b35e67642ce8cdf69a2efa2a078b2a38abbecd
Mike Jandreau <mikejandreau@gmail.com>
2018-01-18 16:50:32 -0500

    Updated file path to skip-link-focus

--- functions.php
+++ functions.php
@@ -136,7 +136,7 @@ function bs4wp_scripts() {
 
 
 
-	wp_enqueue_script( 'bs4wp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'bs4wp-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/vendor/skip-link-focus-fix.js', array(), '20151215', true );
 
 
 
