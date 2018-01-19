19605f7a6cf7501b512139796c31fb1bf60b7b11
Michael R. Jandreau <mrjandre@imac-022279.colby.edu>
2018-01-19 10:35:37 -0500

    Adding hero section for slideshow

--- header.php
+++ header.php
@@ -59,128 +59,124 @@
 
 
 
+	<?php if ( is_front_page() ) : ?>
+
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner" role="listbox">
+				<!-- Slide One - Set the background image for this slide in the line below -->
+				<div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>First Slide</h3>
+						<p>This is a description for the first slide.</p>
+					</div>
+				</div>
+				<!-- Slide Two - Set the background image for this slide in the line below -->
+				<div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Second Slide</h3>
+						<p>This is a description for the second slide.</p>
+					</div>
+				</div>
+				<!-- Slide Three - Set the background image for this slide in the line below -->
+				<div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Third Slide</h3>
+						<p>This is a description for the third slide.</p>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+
+	<?php endif; ?>
+
+
+
+
+
+
 
 
 
 <div class="hero">
-	<div class="other div">
-		<div class=div">
+	<div class="container">
 
-			<?php if ( is_front_page() ) : ?>
+		<?php if ( is_front_page() ) : ?>
 
+			<div class="call-to-action">
+				<?php /* the_title( '<h1 class="entry-title">', '</h1>' ); */ ?>
+				<h1 class="entry-title">Title Goes Here</h1>
+				<p>Base Install is a starter theme for WordPress development based on <a href="http://underscores.me/" target="_blank">Underscores</a>. It features a <a href="http://scribu.net/wordpress/theme-wrappers.html" target="_blank">theme wrapper</a>, a <a href="http://getskeleton.com/" target="_blank">responsive grid</a>, and a powerful <a href="https://labs.ahmadawais.com/WPGulp/" target="_blank">Gulp</a> workflow.</p>
+				<a class="button button-primary" href="https://github.com/mikejandreau/Base-Install" target="_blank">View on GitHub</a><a class="button button-primary" href="https://github.com/mikejandreau/Base-Install/archive/master.zip" download="Base_Install">Download ZIP</a>
+			</div>
 
-
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner" role="listbox">
-          <!-- Slide One - Set the background image for this slide in the line below -->
-          <div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
-            <div class="carousel-caption d-none d-md-block">
-              <h3>First Slide</h3>
-              <p>This is a description for the first slide.</p>
-            </div>
-          </div>
-          <!-- Slide Two - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
-            <div class="carousel-caption d-none d-md-block">
-              <h3>Second Slide</h3>
-              <p>This is a description for the second slide.</p>
-            </div>
-          </div>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
-            <div class="carousel-caption d-none d-md-block">
-              <h3>Third Slide</h3>
-              <p>This is a description for the third slide.</p>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-
-
-
-
-
-
-
-
-
-				
-				<div class="call-to-action">
-					<?php /* the_title( '<h1 class="entry-title">', '</h1>' ); */ ?>
-					<h1 class="entry-title">Kick-start Your WP Dev Workflow</h1>
-					<p>Base Install is a starter theme for WordPress development based on <a href="http://underscores.me/" target="_blank">Underscores</a>. It features a <a href="http://scribu.net/wordpress/theme-wrappers.html" target="_blank">theme wrapper</a>, a <a href="http://getskeleton.com/" target="_blank">responsive grid</a>, and a powerful <a href="https://labs.ahmadawais.com/WPGulp/" target="_blank">Gulp</a> workflow.</p>
-					<a class="button button-primary" href="https://github.com/mikejandreau/Base-Install" target="_blank">View on GitHub</a><a class="button button-primary" href="https://github.com/mikejandreau/Base-Install/archive/master.zip" download="Base_Install">Download ZIP</a>
-				</div>
-
-			<?php elseif ( is_single() ) : ?>
-				<?php the_title( '<h1 class="entry-title"><span class="sr-only">Article Title: </span>', '</h1>' ); ?>
-				<div class="blog-author-date">
-					<span class="blog-date">Posted on <?php echo get_the_date('M j, Y'); ?></span>
-					<?php 
-						$temp_post = get_post($post_id);
-						$user_id = $temp_post->post_author;
-						$user_url = get_author_posts_url($user_id);
-						$first_name = get_the_author_meta('first_name',$user_id);
-						$last_name = get_the_author_meta('last_name',$user_id);
-						$full_name = "{$first_name} {$last_name}";
-						echo '<span class="blog-author">by <a href="' . $user_url . '">' . $full_name . '</a></span>';
-					?>
-				</div>
-
-			<?php elseif ( is_author() ) : ?>
-				<?php
+		<?php elseif ( is_single() ) : ?>
+			<?php the_title( '<h1 class="entry-title"><span class="sr-only">Article Title: </span>', '</h1>' ); ?>
+			<div class="blog-author-date">
+				<span class="blog-date">Posted on <?php echo get_the_date('M j, Y'); ?></span>
+				<?php 
 					$temp_post = get_post($post_id);
 					$user_id = $temp_post->post_author;
+					$user_url = get_author_posts_url($user_id);
 					$first_name = get_the_author_meta('first_name',$user_id);
 					$last_name = get_the_author_meta('last_name',$user_id);
 					$full_name = "{$first_name} {$last_name}";
-					echo '<h1 class="entry-title">Posts by ' . $full_name . ' </h1>';
+					echo '<span class="blog-author">by <a href="' . $user_url . '">' . $full_name . '</a></span>';
 				?>
+			</div>
 
-			<?php elseif ( is_category() ) : ?>
-				<?php single_cat_title( '<h1 class="entry-title">Category: ', '</h1>' ); ?>
+		<?php elseif ( is_author() ) : ?>
+			<?php
+				$temp_post = get_post($post_id);
+				$user_id = $temp_post->post_author;
+				$first_name = get_the_author_meta('first_name',$user_id);
+				$last_name = get_the_author_meta('last_name',$user_id);
+				$full_name = "{$first_name} {$last_name}";
+				echo '<h1 class="entry-title">Posts by ' . $full_name . ' </h1>';
+			?>
 
-			<?php elseif ( is_tag() ) : ?>
-				<?php single_tag_title( '<h1 class="entry-title">Tag: ', '</h1>' ); ?>
+		<?php elseif ( is_category() ) : ?>
+			<?php single_cat_title( '<h1 class="entry-title">Category: ', '</h1>' ); ?>
 
-			<?php elseif ( is_month() ) : ?>
-				<?php the_archive_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php elseif ( is_tag() ) : ?>
+			<?php single_tag_title( '<h1 class="entry-title">Tag: ', '</h1>' ); ?>
 
-			<?php elseif ( is_post_type_archive() ) : ?>
-				<?php post_type_archive_title( '<h1 class="entry-title">Archives: ', '</h1>' ); ?>
+		<?php elseif ( is_month() ) : ?>
+			<?php the_archive_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-			<?php elseif ( is_search() ) : ?>
-				<?php echo '<h1 class="entry-title">Search Results for: ' . get_search_query() .  '</h1>'; ?>
+		<?php elseif ( is_post_type_archive() ) : ?>
+			<?php post_type_archive_title( '<h1 class="entry-title">Archives: ', '</h1>' ); ?>
 
-			<?php elseif ( is_home() ) : ?>
-				<h1>Blog</h1>
+		<?php elseif ( is_search() ) : ?>
+			<?php echo '<h1 class="entry-title">Search Results for: ' . get_search_query() .  '</h1>'; ?>
 
-			<?php elseif ( is_404() ) : ?>
-				<h1>Oops! That page can&rsquo;t be found.</h1>
+		<?php elseif ( is_home() ) : ?>
+			<h1>Blog</h1>
 
-			<?php elseif ( is_page_template( 'contact.php' ) ) : ?>
-				<h1>Get in Touch</h1>
+		<?php elseif ( is_404() ) : ?>
+			<h1>Oops! That page can&rsquo;t be found.</h1>
 
-			<?php else : ?>
-				<?php the_title( '<h1 class="entry-title"><span class="sr-only">Article Title: </span>', '</h1>' ); ?>
+		<?php elseif ( is_page_template( 'contact.php' ) ) : ?>
+			<h1>Get in Touch</h1>
 
-			<?php endif; ?>
+		<?php else : ?>
+			<?php the_title( '<h1 class="entry-title"><span class="sr-only">Article Title: </span>', '</h1>' ); ?>
 
-		</div><?php // end div ?>
-	</div><?php // end other div ?>
+		<?php endif; ?>
+
+	</div><?php // end .container ?>
 
 	<?php 
 		if ( is_front_page() ) : // show scroll to content button on homepage only
